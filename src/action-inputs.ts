945b35b1cc8376eff4ec9a9842368d632df8715f
Bryce <bryce.reinhard@perkinelmer.com>
2023-03-30 13:58:09 -0500

    Use github env variable instead of my broken one maybe??

--- src/action-inputs.ts
+++ src/action-inputs.ts
@@ -12,12 +12,18 @@ export const getInputs = (): IActionInputs => {
   const WHAT_TO_USE: ESource = (core.getInput('use', { required: false }) as ESource) || ESource.prTitle;
   const JIRA_USER: string = core.getInput('jira-user', { required: true });
   const JIRA_PASSWORD: string = core.getInput('jira-password', { required: true });
-  const REPO_NAME: string = core.getInput('repo-name', { required: false });
+  // const REPO_NAME: string = core.getInput('repo-name', { required: false });
+
+  // this env variable is injected by github
+  // something like = PerkinElmer/sdf-onprem
+  const REPO_NAME: string = core.getInput('github_repository', { required: true });
+
   console.log(`This is repo name from action.inputs: ${REPO_NAME}`);
   console.log(`This is repo name from reading env variable: ${process.env['INPUT_REPO_NAME']}`);
   console.log(`This is repo name from reading env variable: ${process.env['INPUT_REPO-NAME']}`);
   console.log(`This is github.repository name from reading env variable: ${process.env['INPUT_GITHUB.REPOSITORY']}`);
   console.log(`All env variables: ${JSON.stringify(process.env)}`);
+  console.log(`Get github repo name: `);
   return {
     JIRA_TOKEN,
     GITHUB_TOKEN,
